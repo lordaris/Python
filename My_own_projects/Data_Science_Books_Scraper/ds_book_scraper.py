1458b034a4a5360bfda0d9d1d333a02bc5eaf38f
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2022-08-31 10:27:10 -0600

    Update ds_book_scraper.py

--- My_own_projects/Data_Science_Books_Scraper/ds_book_scraper.py
+++ My_own_projects/Data_Science_Books_Scraper/ds_book_scraper.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+"""
+A simple data scraping script to get a data science books list from pdf drive. 
+"""
+
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 "
                          "Safari/537.36"}
 # I used pdfdrive to get book names.
