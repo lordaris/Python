1ccac58c4e38da916e818a48f15738784d5f2712
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2022-08-30 23:32:39 -0600

    Add project

---
+++ My_own_projects/PDFtoText/main.py
@@ -0,0 +1,54 @@
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+import requests
+
+
+path = input("Ingrese un documento PDF: ")
+name = input("Elige un nombre de documento: ")
+
+
+def image_to_text():
+    images = convert_from_path(path)  # Transforma PDF en imágenes
+    for i in range(len(images)):  # guarda las imágenes de PDF
+        images[i].save(name + str(i) + ".jpg", "JPEG")
+
+    for i in range(len(images)):  # Lee todas las imágenes creadas
+        img = Image.open(f"{name}{i}.jpg")
+        img.load()
+        text = pytesseract.image_to_string(img, lang="spa")  # Convierte las imágenes en texto
+        # print(text)
+        with open(f"{name}.txt", "a", encoding="utf-8") as document:  # Guarda las imágenes en un archivo
+            document.writelines(text)
+
+
+image_to_text()
+
+# La API de BIONIC READING
+
+
+def bionic_reading():
+    with open(f"{name}.txt", "r", encoding="utf-8") as f:
+        lines = f.read()
+        print(lines)
+
+        url = "https://bionic-reading1.p.rapidapi.com/convert"
+
+        payload = f"content={lines}&response_type=html&request_type=html&fixation=1&saccade=10"
+        headers = {
+            "content-type": "application/x-www-form-urlencoded; charset=utf-8",
+            "X-RapidAPI-Key": "e078277099msha617702d90e7263p1e14a8jsn643fe812ba7c",
+            "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
+        }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+        x = response.text
+
+        with open(f"{name}.html", "w", encoding="utf-8") as htmldocument:
+            htmldocument.writelines(x)
+
+# Eliminar cuando funcione bien
+    print(response.text)
+
+
+bionic_reading()
