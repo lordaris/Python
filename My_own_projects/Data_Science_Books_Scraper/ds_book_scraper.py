2d8a328d8ba666cbc2aa49c35723aa22c34b552e
Jesús Armando Peña Lucero <audio.aris@gmail.com>
2022-08-31 10:23:24 -0600

    Update dsbooks scraper

---
+++ My_own_projects/Data_Science_Books_Scraper/ds_book_scraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 "
+                         "Safari/537.36"}
+# I used pdfdrive to get book names.
+response = requests.get("https://www.pdfdrive.com/data-science-books.html", headers=headers)
+webpage = response.content
+
+soup = BeautifulSoup(webpage, "html.parser")
+books = {}
+
+# If you used a different website to get book names, you may need different tags.
+for parent in soup.find_all("ul"):
+    for n, tag in enumerate(parent.find_all("li")):
+        title = [x for x in tag.find_all(class_="ai-search")]
+        for item in title:
+            books[item.text.strip()] = 0
+print(books)
